8748338e02a36c86b55871f385cabad70ff20d15
Prathamesh.Patil <Prathamesh.Patil@connectwise.com>
2026-02-08 20:29:55 +0530

    add migrations

---
+++ EF_DataAccess/Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using EF_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_DataAccess.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=patil123;Host=localhost;Port=5432;Database=EF;");
+        }
+    }
+}
